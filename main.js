11f5b6d8398ccb079beb6c23249b7dbb22d60300
Shabbeer Khan <patanzshabbir@gmail.com>
2023-12-17 14:36:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const changeBgToGrey =() => {
+  document.getElementById("button1").style.backgroundColor ="grey";
+   document.getElementById("paraColor").style.backgroundColor ="grey";
+   document.getElementById("card").style.backgroundColor ="grey";
+}
+
+const changeBgToGreen = () =>{
+  document.getElementById("button2").style.backgroundColor ="green";
+  document.getElementById("paraColor").style.backgroundColor ="green";
+  document.getElementById("card").style.backgroundColor ="green";
+}
+
+const changeBgToBlue =()=> {
+  document.getElementById("button3").style.backgroundColor ="blue";
+  document.getElementById("paraColor").style.backgroundColor ="blue";
+      document.getElementById("card").style.backgroundColor ="blue";
+}
+
+const changeBgToPink =() =>{
+  document.getElementById("button4").style.backgroundColor ="pink";
+   document.getElementById("paraColor").style.backgroundColor ="pink";
+    document.getElementById("card").style.backgroundColor ="pink";
+}
